0b516f5fa5473e12f33fff07ef91227e97f89283
Akshat4519 <aks.mish@yahoo.com>
2017-02-03 16:24:20 +0530

    Add files via upload

--- EmployeesInfoWCF/IService1.cs
+++ EmployeesInfoWCF/IService1.cs
@@ -5,43 +5,53 @@ using System.ServiceModel.Web;
 
 namespace EmployeesInfoWCF
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-    [ServiceContract]
-    public interface IService1
-    {
-        [OperationContract]
-        [WebGet(RequestFormat = WebMessageFormat.Json, 
-            ResponseFormat = WebMessageFormat.Json, 
-            UriTemplate = "/GetData/")]
-        List<Employee> GetEmpData();
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+	[ServiceContract]
+	public interface IService1
+	{
+		[OperationContract]
+		[WebGet(RequestFormat = WebMessageFormat.Json,
+			ResponseFormat = WebMessageFormat.Json,
+			UriTemplate = "/GetData/")]
+		List<Employee> GetEmpData();
 
 		[OperationContract]
-		[WebInvoke(UriTemplate = "/AddEmployee/")]
-		void AddEmployee();
-    }
+		[WebInvoke(Method = "POST",
+			ResponseFormat = WebMessageFormat.Json,
+			RequestFormat = WebMessageFormat.Json,
+			UriTemplate = "/SaveData/")]
+		void SaveData(Employee emp);
+
+		[OperationContract]
+		[WebInvoke(UriTemplate = "/AddData/")]
+		void AddData();
+	}
 
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class Employee
-    {
-        [DataMember]
-        public string Id
-        {
-            get; set;
-        }
+	// Use a data contract as illustrated in the sample below to add composite types to service operations.
+	[DataContract]
+	public class Employee
+	{
+		[DataMember]
+		public string Id
+		{
+			get;
+			set;
+		}
 
-        [DataMember]
-        public string Name
-        {
-            get; set;
-        }
+		[DataMember]
+		public string Name
+		{
+			get;
+			set;
+		}
 
 
-        [DataMember]
-        public string Salary
-        {
-            get; set;
-        }
-    }
+		[DataMember]
+		public string Salary
+		{
+			get;
+			set;
+		}
+	}
 }
--- EmployeesInfoWCF/Service1.svc.cs
+++ EmployeesInfoWCF/Service1.svc.cs
@@ -9,57 +9,87 @@ using System.Text;
 
 namespace EmployeesInfoWCF
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
-    {
-        public List<Employee> GetEmpData()
-        {
-            string query = "Select * from Employees";
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IService1
+	{
+		public List<Employee> GetEmpData()
+		{
+			string connectionString = @"Data Source = (LocalDB)\Local; Initial Catalog = EmployeeInfo; Integrated Security = True";
+			
+			string query = "Select * from Employees";
 
-            string connectionString = @"Data Source = (LocalDB)\Local; Initial Catalog = EmployeeInfo; Integrated Security = True";
+			List<Employee> emp = new List<Employee>();
 
-            List<Employee> emp = new List<Employee>();
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					using (SqlCommand cmd = new SqlCommand(query, con))
+					{
+						con.Open();
+						using (SqlDataReader reader = cmd.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								Employee tempEmp = new Employee
+								{
+									Id = reader["Id"].ToString(),
+									Name = reader["Name"].ToString(),
+									Salary = reader["Salary"].ToString()
+								};
+								emp.Add(tempEmp);
+							}
+						}
+						con.Close();
+					}
+				}
+			}
+			catch (SqlException)
+			{
+				throw;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+			return emp;
+		}
 
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connectionString))
-                {
-                    con.Open();
+		public void SaveData(Employee emp)
+		{
+			string connectionString = @"Data Source = (LocalDB)\Local; Initial Catalog = EmployeeInfo; Integrated Security = True";
 
+			string query = "Update Employees Set Name= @Name, Salary= @Salary where Id= @Id";
 
-                    using (SqlCommand cmd = new SqlCommand(query, con))
-                    {
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Employee tempEmp = new Employee
-                                {
-                                    Id = reader["Id"].ToString(),
-                                    Name = reader["Name"].ToString(),
-                                    Salary = reader["Salary"].ToString()
-                                };
-                                emp.Add(tempEmp);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-            return emp;
-        }
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					using (SqlCommand cmd = new SqlCommand(query, con))
+					{
+						cmd.Parameters.AddWithValue("@Name", emp.Name);
+						cmd.Parameters.AddWithValue("@Salary", emp.Salary);
+						cmd.Parameters.AddWithValue("@Id", emp.Id);
+						con.Open();
+						cmd.ExecuteNonQuery();
+						con.Close();
+					}
+				}
+			}
+			catch (SqlException)
+			{
+				throw;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
 
-		public void AddEmployee()
+		public void AddData()
 		{
 
 		}
-    }
+	}
 }
