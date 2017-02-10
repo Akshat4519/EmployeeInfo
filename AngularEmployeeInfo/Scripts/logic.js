2e80dff5b33d220db18f82f18b4cc9f1470473cc
Akshat4519 <aks.mish@yahoo.com>
2017-02-10 16:17:29 +0530

    Add files via upload

--- AngularEmployeeInfo/Scripts/logic.js
+++ AngularEmployeeInfo/Scripts/logic.js
@@ -3,8 +3,7 @@ var infoDisplayed = 0;
 var toRefresh = 0;
 var finalRowCount = 0;
 $(document).ready(function () {
-    feedLocalStorage();
-    if (sessionStorage.validUser == "false") {
+    if (sessionStorage.validUser == "false" || sessionStorage.validUser == undefined) {
         window.location("Login.html");
         return;
     }
@@ -322,64 +321,4 @@ function saveFailed(msg) {
     $('#inputInfo').css("color", "red");
     $('#inputInfo').text("Error saving data: " + msg.status + " - " + msg.statusText);
     $("#inputInfo").show();
-}
-
-function feedLocalStorage() {
-    var sessionStorage_transfer = function (event) {
-        if (!event) { event = window.event; } // ie suq
-        if (!event.newValue) return;          // do nothing if no value to work with
-        if (event.key == 'getSessionStorage') {
-            // another tab asked for the sessionStorage -> send it
-            localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
-            // the other tab should now have it, so we're done with it.
-            localStorage.removeItem('sessionStorage'); // <- could do short timeout as well.
-        } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
-            // another tab sent data <- get it
-            var data = JSON.parse(event.newValue);
-            for (var key in data) {
-                sessionStorage.setItem(key, data[key]);
-            }
-        }
-    };
-
-    // listen for changes to localStorage
-    if (window.addEventListener) {
-        window.addEventListener("storage", sessionStorage_transfer, false);
-    } else {
-        window.attachEvent("onstorage", sessionStorage_transfer);
-    };
-
-
-    // Ask other tabs for session storage (this is ONLY to trigger event)
-    if (!sessionStorage.length) {
-        localStorage.setItem('getSessionStorage', 'foobar');
-        localStorage.removeItem('getSessionStorage', 'foobar');
-    };
-
-    //if (!sessionStorage.length) {
-    //    // Ask other tabs for session storage
-    //    localStorage.validUser = false;
-    //};
-
-    //window.addEventListener('storage', function (event) {
-
-    //    //console.log('storage event', event);
-
-    //    if (event.key == 'validUser') {
-    //        // Some tab asked for the sessionStorage -> send it
-
-    //        localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
-    //        localStorage.removeItem('sessionStorage');
-
-    //    } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
-    //        // sessionStorage is empty -> fill it
-
-    //        var data = JSON.parse(event.newValue),
-    //                    value;
-
-    //        for (key in data) {
-    //            sessionStorage.setItem(key, data[key]);
-    //        }
-    //    }
-    //});
 }
--- AngularEmployeeInfo/Scripts/login.js
+++ AngularEmployeeInfo/Scripts/login.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
-    feedLocalStorage();
+    if (sessionStorage.validUser == "true") {
+        window.location("Index.html");
+        return;
+    }
     $("input").keypress(function (e) {
         if (e.keyCode == 13) {
             AuthenticateUser();
@@ -62,66 +65,4 @@ function AuthenticateUser() {
         $("[name='psw']").focus();
         return;
     }
-    //feedLocalStorage();
-}
-
-function feedLocalStorage() {
-    var sessionStorage_transfer = function (event) {
-        if (!event) { event = window.event; } // ie suq
-        if (!event.newValue) return;          // do nothing if no value to work with
-        if (event.key == 'getSessionStorage') {
-            // another tab asked for the sessionStorage -> send it
-            localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
-            // the other tab should now have it, so we're done with it.
-            localStorage.removeItem('sessionStorage'); // <- could do short timeout as well.
-        } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
-            // another tab sent data <- get it
-            var data = JSON.parse(event.newValue);
-            for (var key in data) {
-                sessionStorage.setItem(key, data[key]);
-            }
-        }
-    };
-
-    // listen for changes to localStorage
-    if (window.addEventListener) {
-        window.addEventListener("storage", sessionStorage_transfer, false);
-    } else {
-        window.attachEvent("onstorage", sessionStorage_transfer);
-    };
-
-
-    // Ask other tabs for session storage (this is ONLY to trigger event)
-    if (!sessionStorage.length) {
-        localStorage.setItem('getSessionStorage', 'foobar');
-        localStorage.removeItem('getSessionStorage', 'foobar');
-    };
-
-
-    //if (!sessionStorage.length) {
-    //    // Ask other tabs for session storage
-    //    localStorage.validUser = false;
-    //};
-
-    //window.addEventListener('storage', function (event) {
-
-    //    //console.log('storage event', event);
-
-    //    if (event.key == 'validUser') {
-    //        // Some tab asked for the sessionStorage -> send it
-
-    //        localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
-    //        localStorage.removeItem('sessionStorage');
-
-    //    } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
-    //        // sessionStorage is empty -> fill it
-
-    //        var data = JSON.parse(event.newValue),
-    //                    value;
-
-    //        for (key in data) {
-    //            sessionStorage.setItem(key, data[key]);
-    //        }
-    //    }
-    //});
 }
